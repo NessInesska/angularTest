93894688566de08ba1010245cb4aee6cf7a11818
Inessa Borodich <38314470+NessInesska@users.noreply.github.com>
2018-12-16 15:06:42 +0400

    Refactoring for project arcitechture (#2)

--- src/app/components/hero-detail/hero-detail.component.ts
+++ src/app/components/hero-detail/hero-detail.component.ts
@@ -2,8 +2,8 @@ import { Component, OnInit, Input } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
 
-import { Hero }         from '../hero';
-import { HeroService }  from '../hero.service';
+import { Hero } from '../../classes/hero';
+import { HeroService } from '../../services';
 
 @Component({
   selector: 'app-hero-detail',
@@ -11,25 +11,25 @@ import { HeroService }  from '../hero.service';
   styleUrls: [ './hero-detail.component.css' ]
 })
 export class HeroDetailComponent implements OnInit {
+
   @Input() hero: Hero;
 
-  constructor(
-    private route: ActivatedRoute,
-    private heroService: HeroService,
-    private location: Location
-  ) {}
+  constructor(private route: ActivatedRoute,
+              private heroService: HeroService,
+              private location: Location) {
+  }
 
-  ngOnInit(): void {
+  public ngOnInit(): void {
     this.getHero();
   }
 
-  getHero(): void {
+  public getHero(): void {
     const id = +this.route.snapshot.paramMap.get('id');
     this.heroService.getHero(id)
       .subscribe(hero => this.hero = hero);
   }
 
-  goBack(): void {
+  public goBack(): void {
     this.location.back();
   }
 }
