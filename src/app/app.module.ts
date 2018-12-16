a6938d5ddb84347918572781f00db59228814563
Inessa Borodich <inesska@MacBook-Air-Inessa.local>
2018-12-16 16:45:18 +0400

    Add http requests and in-memory-data-service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import {HttpClientInMemoryWebApiModule} from 'angular-in-memory-web-api';
 
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './components/heroes';
@@ -9,6 +10,8 @@ import { HeroDetailComponent } from './components/hero-detail';
 import { MessagesComponent } from './components/messages';
 import { AppRoutingModule } from './app-routing.module';
 import { DashboardComponent } from './components/dashboard';
+import { InMemoryDataService } from './services';
+import { HeroSearchComponent } from './hero-search/hero-search.component'
 
 @NgModule({
   declarations: [
@@ -16,13 +19,17 @@ import { DashboardComponent } from './components/dashboard';
     HeroesComponent,
     HeroDetailComponent,
     MessagesComponent,
-    DashboardComponent
+    DashboardComponent,
+    HeroSearchComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     AppRoutingModule,
     HttpClientModule,
+    HttpClientInMemoryWebApiModule.forRoot(
+      InMemoryDataService, { dataEncapsulation: false }
+    )
   ],
 
   bootstrap: [
--- src/app/components/hero-detail/hero-detail.component.ts
+++ src/app/components/hero-detail/hero-detail.component.ts
@@ -32,4 +32,9 @@ export class HeroDetailComponent implements OnInit {
   public goBack(): void {
     this.location.back();
   }
+
+  public save(): void {
+    this.heroService.updateHero(this.hero)
+      .subscribe(() => this.goBack());
+  }
 }
